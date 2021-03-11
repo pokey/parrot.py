89babe6a200857bef1fe3a787ad6659d3683866d
Pokey Rule <pokey.rule@gmail.com>
2021-03-11 21:12:11 +0000

    Remove unnecessary talon imports

--- lib/modes/base_talon_mode.py
+++ lib/modes/base_talon_mode.py
@@ -10,10 +10,6 @@ class BaseTalonMode(BaseMode, metaclass=ABCMeta):
     def start(self): 
         talon_repl_path = expanduser("~/.talon/bin/repl")
         self.talon_subprocess = Popen([talon_repl_path, "-wr"], stdin=PIPE)
-        self.talon_subprocess.stdin.write(
-            "from talon import actions\n".encode("utf-8")
-        )
-        self.talon_subprocess.stdin.flush()
         super().start()
 
     def handle_sounds( self, dataDicts ):
